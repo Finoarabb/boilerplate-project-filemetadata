5d97710c938ae3592a68788daae0deab03a924f3
Finoarabb <msirojuddin26@gmail.com>
2025-05-19 17:17:02 +0700

    hope it fix

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', function (req, res) {
 app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
   console.log(req.file);
   const data = {
-    name:req.file.filename,
+    name:req.file.originalname,
     type:req.file.mimetype,
     size:req.file.size
   }
